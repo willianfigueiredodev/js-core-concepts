7411ac8a542565439a437ef90189c89c6e9ca7b2
Willian Figueiredo <willianfigueiredodev@gmail.com>
2026-03-23 22:20:41 -0300

    feat: resolução exercicio 4, 5 e 6.

---
+++ ex03.js
@@ -0,0 +1,10 @@
+// Crie um programa que receba um número e retorne seu valor absoluto.
+
+function valorAbsoluto(number) {
+  if (number < 0) {
+    return number * -1;
+  } 
+    return number;
+}
+
+console.log(valorAbsoluto(-6));
---
+++ ex05.js
@@ -0,0 +1,8 @@
+// Crie um programa que receba três números e retorne a média.
+
+function mediaNumeros (valueA, valueB, valueC){
+   const soma = valueA + valueB + valueC;
+    return soma / 3;
+}
+
+console.log(mediaNumeros(3, 10, 8));
