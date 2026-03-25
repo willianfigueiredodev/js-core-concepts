e5bc2eb8c7c6bd23efc904e8434d2207cc45bda1
Willian Figueiredo <willianfigueiredodev@gmail.com>
2026-03-24 22:25:29 -0300

    chore: remove root duplicates after merge

--- ex05.js
+++
@@ -1,8 +0,0 @@
-// Crie um programa que receba três números e retorne a média.
-
-function mediaNumeros (valueA, valueB, valueC){
-   const soma = valueA + valueB + valueC;
-    return soma / 3;
-}
-
-console.log(mediaNumeros(3, 10, 8));
